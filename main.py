f23682e8feb807a7daa3cf39da93d431761ab1b0
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-08-27 11:35:33 +0300

    flask 2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'Hello, world'
+    return render_template('base.html')
+
 
 # if __name__ == '__main':
 app.run(port = 8080)
